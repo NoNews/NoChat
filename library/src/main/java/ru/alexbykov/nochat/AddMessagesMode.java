2971dd69a001609c54e1b26f082906cde9344119
Alex Bykov <me@alexbykov.ru>
2017-10-20 15:27:07 +0300

    message mode enum

---
+++ library/src/main/java/ru/alexbykov/nochat/AddMessagesMode.java
@@ -0,0 +1,15 @@
+package ru.alexbykov.nochat;
+
+/**
+ * @author Alex Bykov
+ *         Date: 18.10.17.
+ *         <p>
+ *         You can contact me at me@alexbykov.ru
+ */
+
+public enum AddMessagesMode {
+
+    TO_END,
+    TO_START,
+    INSTEAD_ALL
+}
