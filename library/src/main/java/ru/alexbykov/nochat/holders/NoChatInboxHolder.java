9ec00a9ea9ed623476c31fc8de732f07eb1586ae
Alex Bykov <me@alexbykov.ru>
2017-10-27 17:38:45 +0300

    some fixes

--- library/src/main/java/ru/alexbykov/nochat/holders/NoChatInboxHolder.java
+++ library/src/main/java/ru/alexbykov/nochat/holders/NoChatInboxHolder.java
@@ -2,6 +2,8 @@ package ru.alexbykov.nochat.holders;
 
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import ru.alexbykov.nochat.R;
@@ -22,6 +24,8 @@ public class NoChatInboxHolder extends NoChatBaseViewHolder {
     public TextView tvName;
     public ImageView civProfileImage;
     public ImageView ivImage;
+    public RelativeLayout ltMessageRoot;
+    public LinearLayout ltMessage;
 
 
     public NoChatInboxHolder(View itemView) {
@@ -31,5 +35,7 @@ public class NoChatInboxHolder extends NoChatBaseViewHolder {
         tvName = bindView(R.id.tv_name);
         ivImage = bindView(R.id.iv_photo);
         civProfileImage = bindView(R.id.civ_profile_image);
+        ltMessageRoot =bindView(R.id.lt_message_root);
+        ltMessage=bindView(R.id.lt_message);
     }
 }
--- library/src/main/java/ru/alexbykov/nochat/holders/NoChatOutboxHolder.java
+++ library/src/main/java/ru/alexbykov/nochat/holders/NoChatOutboxHolder.java
@@ -2,6 +2,8 @@ package ru.alexbykov.nochat.holders;
 
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import ru.alexbykov.nochat.R;
@@ -21,11 +23,15 @@ public class NoChatOutboxHolder extends NoChatBaseViewHolder {
     public TextView tvName;
     public ImageView civPhoto;
     public ImageView ivPhoto;
+    public RelativeLayout ltMessageRoot;
+    public LinearLayout ltMessage;
 
     public NoChatOutboxHolder(View itemView) {
         super(itemView);
         tvMessage = bindView(R.id.tv_message);
         tvDate = bindView(R.id.tv_date);
         ivPhoto = bindView(R.id.iv_photo);
+        ltMessageRoot = bindView(R.id.lt_message_root);
+        ltMessage = bindView(R.id.lt_message);
     }
 }
